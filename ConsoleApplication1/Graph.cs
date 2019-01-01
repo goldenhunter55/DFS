3fdab400bbfdbc6419544fea391c8ab1601d5640
ahmedmagdy55 <ahmedmagdy_619@yahoo.com>
2019-01-01 20:09:50 +0200

    Add files via upload

---
+++ ConsoleApplication1/Graph.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Graph
+    {
+        int V;
+        List<List<int>> adjList;
+
+        public Graph(int V)
+        {
+            this.V = V;
+            adjList = new List<List<int>>();
+            for (int i = 0; i < V; i++)
+            {
+                adjList.Add(new List<int>());
+            }
+        }
+        public void addEdge(int v, int edge)
+        {
+            adjList[v].Add(edge);
+        }
+
+        public void BFS(int S)
+        {
+            bool[] Visited = new bool[V];
+            for (int i = 0; i < V; i++)
+            {
+                Visited[i] = false;
+            }
+            Visited[S] = true;
+            Queue<int> Q = new Queue<int>();
+            Q.Enqueue(S);
+
+            while (Q.Count > 0)
+            {
+                S = Q.Dequeue();
+                Console.Write(S + " ");
+                for (int i = 0; i < adjList[S].Count; i++)
+                {
+                    if (Visited[adjList[S][i]] == false)
+                    {
+                        Visited[adjList[S][i]] = true;
+                        Q.Enqueue(adjList[S][i]);
+                    }
+                }
+            }
+        }
+
+        public void DFSUtill(int v, bool[] visited)
+        {
+            visited[v] = true;
+            Console.Write(v + " ");
+
+            for (int i = 0; i < adjList[v].Count; i++)
+            {
+                int j = adjList[v][i];
+                if (visited[j] == false)
+                {
+                    DFSUtill(j, visited);
+                }
+            }
+        }
+        public void DFS(int s)
+        {
+            bool [] Visited=new bool [V];
+
+            DFSUtill(s, Visited);
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+
+
+        static int LCS(string P, string Q, int N, int M)
+        {
+            if (N == 0 || M == 0) { return 0; }
+            else if (P[N] == Q[M])
+            {
+                return (1 + LCS(P, Q, N - 1, M - 1));
+            }
+            else
+            {
+
+                int tmp1 = LCS(P, Q, N - 1, M);
+                int tmp2 = LCS(P, Q, N, M - 1);
+                return Math.Max(tmp1, tmp2);
+            }
+            // O(2^(N+M))
+        }
+
+
+        // bottom up uproach 
+
+        static int LCS_DP(string P, string Q, int N, int M)
+        {
+            int[,] Value = new int[N + 1, M + 1];
+            for (int i = 0; i <= N; i++)
+            {
+                for (int j = 0; j <= M; j++)
+                {
+                    if (i == 0 || j == 0) { Value[i, j] = 0; }
+                    else if (P[i - 1] == Q[j - 1])
+                    {
+                        Value[i, j] = 1 + Value[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        Value[i, j] = Math.Max(Value[i - 1, j], Value[i, j - 1]);
+                    }
+
+                }
+            }
+            return Value[N, M];
+            //O(N*M)
+        }
+
+
+
+        static int knapsack(int[] value,int [] Weight, int n,int c)
+        {
+            int[,] DP = new int[n + 1, c + 1];
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= c; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        DP[i, j] = 0;
+                    }
+                    else if(n>c){
+                        DP[i,j]=DP[i-1,j];
+                    }
+                    else {
+                        int temp1 = DP[i - 1, j];
+                        int temp2 = value[i - 1] + DP[i - 1, j - Weight[i - 1]];
+                        DP[i, j] = Math.Max(temp1, temp2);
+                    }
+        
+                }
+               
+            }
+            return DP[n, c];
+        }
+      
+        static void Main(string[] args)
+        {
+
+            Graph g = new Graph(4);
+            g.addEdge(0, 1);
+            g.addEdge(1, 1);
+            g.addEdge(1, 2);
+            g.addEdge(1, 3);
+            g.addEdge(2, 0);
+            g.addEdge(2, 3);
+            g.addEdge(3, 2);
+
+            g.DFS(3);
+          
+        }
+    }
+}
